ceab5b85f1edf754ac98fe518b06f2e9ecd62b5c
willwearing <willwearing@gmail.com>
2020-11-03 13:11:39 -0700

    mvp done

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const helmet = require("helmet");
 
 const carRouter = require("../cars/cars-router");
-const db = require("../data/connection.js");
+// const db = require("../data/connection.js");
 const server = express();
 
 server.use(helmet());
@@ -14,16 +14,6 @@ server.use("/api/cars", carRouter);
 
 server.get("/", (req, res) => {
   res.json("Server is up");
-});
-
-server.get("/api/cars", (req, res) => {
-  db("cars")
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
 });
 
 module.exports = server;
--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,5 +1,44 @@
 const express = require("express");
-
+const db = require("../data/connection");
 const router = express.Router();
-
+const Cars = {
+  getAll() {
+    return db("cars");
+  },
+  getById(id) {
+    return db("cars").where({ id }).first();
+  },
+  create(post) {
+    return db("cars")
+      .insert(post, "id")
+      .then(([id]) => this.getById(id));
+  },
+  update(id, post) {
+    return db("cars")
+      .where({ id })
+      .update(post)
+      .then((count) => (count > 0 ? this.getById(id) : null));
+  },
+  delete(id) {
+    return db("cars").where({ id }).del();
+  },
+};
+router.get("/", (req, res) => {
+  Cars.getAll()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+router.post("/", (req, res) => {
+  Cars.create(req.body)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
 module.exports = router;
